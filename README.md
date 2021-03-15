028625c6c37a4133dbcd325702c6859983441005
Abhyuday Tripathi <tripathi.abhyuday2010@gmail.com>
2021-03-15 23:43:04 +0530

    Add main.py

---
+++ README.md
@@ -0,0 +1,19 @@
+# Gif Converter Python
+
+## How to use -
+
+```sh
+  pip3 install -r requierments.txt
+```
+
+and then just add a video with a .mp4 extension and it should be less that 3 secs
+
+and after all that just run --
+
+```sh
+  python3 main.py
+```
+
+License - MIT
+
+by - Abhyuday Tripathi
---
+++ main.py
@@ -0,0 +1,16 @@
+# import all the packages
+import imageio # pip3 install imageio imageio-ffmpeg
+import os
+clip = os.path.abspath("me.mp4")
+def gif_maker(input_path, target_format):
+  output_path = os.path.splitext(input_path)[0] + target_format
+  print(f"Converting {input_path} \n to {output_path}")
+  reader = imageio.get_reader(input_path)
+  fps = reader.get_meta_data()['fps']
+  writer = imageio.get_writer(output_path, fps=fps)
+  for frames in reader:
+    writer.append_data(frames)
+    print(f"Frame {frames}")
+  print("Done!")  
+  writer.close()
+gif_maker(clip, '.mov')
